48ede893a86531e8985f0517795d651c18b0f78e
prince6730 <65157993+prince6730@users.noreply.github.com>
2021-01-04 18:29:49 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <div class="container">
+            copyright &copy; by <a href="http://newsfeeder.com">News Feeder</a> All Right Reserved 2020.
+        </div>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ include/top.php
@@ -0,0 +1,29 @@
+<?php 
+ob_start(); 
+require_once('include/db.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>News Feeder</title>
+
+    <!-- Bootstrap -->
+    <link rel="icon" type="image/jpg" href="img/favicon1.jpg">
+    <link rel="stylesheet" href="css/font-awesome.css">
+     <link rel="stylesheet" href="css/animate.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500;600&display=swap" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+   
+                                 
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
