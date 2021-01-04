5c75ba1ff2226e4dbccc22071fd1f467ee5b67e9
prince6730 <65157993+prince6730@users.noreply.github.com>
2021-01-04 18:15:26 +0530

    Create footer.php

---
+++ admin/include/footer.php
@@ -0,0 +1,96 @@
+    <footer class="text-center">
+        copyright &copy; by <a href="">News Feeder</a> from 2019 - <?php echo date('Y');?>
+    </footer>
+    
+   </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+    <script src="js/code.js"></script>
+    <script>
+    tinymce.init({
+  selector: "textarea#textarea",
+  height: 300,
+  plugins: [
+    "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+    "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+    "table contextmenu directionality emoticons template textcolor paste fullpage textcolor colorpicker textpattern"
+  ],
+
+  toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+  toolbar2: "cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | insertdatetime preview | forecolor backcolor",
+  toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft",
+
+  menubar: false,
+  toolbar_items_size: 'small',
+
+  style_formats: [{
+    title: 'Bold text',
+    inline: 'b'
+  }, {
+    title: 'Red text',
+    inline: 'span',
+    styles: {
+      color: '#ff0000'
+    }
+  }, {
+    title: 'Red header',
+    block: 'h1',
+    styles: {
+      color: '#ff0000'
+    }
+  }, {
+    title: 'Example 1',
+    inline: 'span',
+    classes: 'example1'
+  }, {
+    title: 'Example 2',
+    inline: 'span',
+    classes: 'example2'
+  }, {
+    title: 'Table styles'
+  }, {
+    title: 'Table row 1',
+    selector: 'tr',
+    classes: 'tablerow1'
+  }],
+
+  templates: [{
+    title: 'Test template 1',
+    content: 'Test 1'
+  }, {
+    title: 'Test template 2',
+    content: 'Test 2'
+  }],
+  content_css: [
+    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+    '//www.tinymce.com/css/codepen.min.css'
+  ],
+    
+    <?php
+    $media_query = "SELECT * FROM media ORDER BY id DESC";
+      $media_run =mysqli_query($con, $media_query);
+      if(mysqli_num_rows($media_run) > 0){
+          
+      
+    ?>
+    
+    image_list: [
+        <?php
+          while($media_row = mysqli_fetch_array($media_run)){
+              $media_name = $media_row['image'];
+              
+          ?>
+ {title: '<?php echo $media_name;?>', value: 'media/<?php echo $media_name;?>'},
+        
+        <?php } ?>
+  ]          
+     <?php } ?>
+});
+
+   </script>
+
+  </body>
+</html>
